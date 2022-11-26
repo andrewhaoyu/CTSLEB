08013590aabb7854c74d53078d2511082990fb08
dorks81 <dayne0208@gmail.com>
2022-11-26 06:29:14 -1000

    initial commit

---
+++ R/helper_CreateQRange.R
@@ -0,0 +1,17 @@
+#' Helper function for PreparePlinkFile()
+#' @description
+#' Calls CreateQRange() using either the default pthres or user input values
+#' Not exported
+#' @param x pthres
+#' @return CreateQRange() object
+#'
+helper_CreateQRange <- function(x)
+{
+  if (is.null(pthres)) {
+    pthres <- c(5E-08,5E-07,5E-06,5E-05,5E-04,5E-03,
+                5E-02,5E-01,1.0)
+    print(paste0("pthres is NULL...using default values "))
+  }
+  q_range <- CreateQRange(pthres)
+  return (q_range)
+}
