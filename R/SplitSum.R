d81f543196050fc0460a2de0795777a7a82d3e6d
dorks81 <dayne0208@gmail.com>
2022-12-26 12:42:45 -0500

    add write_tables option

--- R/SplitSum.R
+++ R/SplitSum.R
@@ -5,6 +5,7 @@
 #' population smaller than the ref population.
 #'
 #' @param x sum_com data.frame object produced by AlignSum().
+#' @param write_tables
 #' @return  A list with the two data.frame objects of SNPs with p-values in the
 #' ref population smaller than the target population in the [[1]] slot.
 #' SNPs with p-values in the target population smaller than the ref population
@@ -14,26 +15,39 @@
 #' sum_com <- AlignSum(sum_tar = sum_AFR, sum_other = sum_EUR)
 #' split_list <- SplitSum(sum_com)
 
-SplitSum <- function(x){
-  sum_com <- x
-  sum_com_select <- sum_com %>%
+SplitSum <- function(x,
+                     results_dir,
+                     write_tables = TRUE){
+  sum.com <- x
+  sum.com_select <- sum.com %>%
     mutate(split_ind =
              ifelse(
                (P<P_ref)|is.na(P_ref),T,F)
     )%>%
     select(SNP,P,P_ref,split_ind)
 
-  sum_com_select_other_ref <- sum_com_select %>%
+  sum.com_select_other_ref <- sum.com_select %>%
     filter(split_ind==F) %>%
     select(SNP,P_ref) %>%
     rename(P = P_ref)
 
-  sum_com_select_tar_ref <- sum_com_select %>%
+  sum.com_select_tar_ref <- sum.com_select %>%
     filter(split_ind==T) %>%
     select(SNP,P)
 
-  sum_list <- list(sum_com_select_other_ref,
-                   sum_com_select_tar_ref)
+  sum_list <- list(sum.com_select_other_ref,
+                   sum.com_select_tar_ref)
+  if (write_tables) {
+    write_list <- WriteSplitTables(x = sum_list,
+                             results_dir = results_dir,
+                             ref_split_file = "sum_ref.txt",
+                             target_split_file = "sum_target.txt")
+
+    return(write_list)
+  } else {
+    print(paste0("WriteSplitTables() was not performed"))
+    return(sum_list)
+  }
 
   return(sum_list)
 
