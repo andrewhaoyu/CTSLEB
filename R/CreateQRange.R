981abbccf5b1eef137c0831dde67853b08a13061
dorks81 <dayne0208@gmail.com>
2022-12-29 07:23:08 -0500

    basic options only

--- R/CreateQRange.R
+++ R/CreateQRange.R
@@ -1,12 +1,21 @@
-#' Create the q range file used for plink2
+#' Create q_range object for PreparePlinkFile()
+#' @description
+#' Calls CreateQRange() with either the default pthres or user input values
+#' Not exported
+#' @param x pthres
+#' @return CreateQRange() object
 #'
-#' @param pthres the p value thresholds for selecting SNPs
-#'
-#' @return the q range file
-#' @export
-#'
-CreateQRange <- function(x){
-  pthres <- x
+CreateQRange <- function(x)
+{
+  print("executing CreateQRange()... ")
+  if (is.null(x)) {
+    pthres <- c(5E-08,5E-07,5E-06,5E-05,5E-04,5E-03,
+                5E-02,5E-01,1.0)
+    print(paste0("pthres is NULL...using default values "))
+  } else {
+    pthres <- x
+  }
+
   n_pthres <- length(pthres)
   q_range <- data.frame(filename = rep("p_tar",n_pthres),
                         small_P = rep(0,n_pthres),
@@ -21,5 +30,6 @@ CreateQRange <- function(x){
     temp = temp+1
   }
   q_range = q_range[1:(temp-1),]
+  print("CreateQRange() complete... ")
   return(q_range)
 }
