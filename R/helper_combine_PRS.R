05a639e9b0eccfa426746d6ce6846f0e2cef64de
dorks81 <dayne0208@gmail.com>
2022-12-29 08:38:43 -0500

    add message

--- R/helper_combine_PRS.R
+++ R/helper_combine_PRS.R
@@ -4,28 +4,28 @@
 #' Not exported
 #' @param scores
 #' @param pthres
-#' @param params_farm
-#' @param out_prefix
+#' @param prs_p_other_
 #' @return 'prs_mat' global variable. Matrix of combined PRSs with clumping and
 #' pthres as column name
 #'
 helper_combine_PRS <- function(scores,
                                pthres,
-                               prs_p_other_,
-                               params_farm=as.null())
+                               prs_p_other_)
   {
-  if (is.null(params_farm)) {
-    print("no params_farm")
-  } else {
-    print("params_farm list will be used")
-    pthres <- as.character(unlist(params_farm["pthres"]))
-  }
+  print("combining PRS results ...")
+  # if (is.null(params_farm)) {
+  #   print("no params_farm")
+  # } else {
+  #   print("params_farm list will be used")
+  #   pthres <- as.character(unlist(params_farm["pthres"]))
+  # }
   prs_list <- list()
   temp <- 1
   names <- colnames(scores[3:ncol(scores)])
   for(k1 in 1:length(pthres)){
     for(k2 in 1:length(pthres)){
       prs_file <- paste0(prs_p_other_,k1,".p_tar_",k2,".sscore")
+      print(paste0("reading :", prs_file))
       prs_temp <- fread(prs_file)
       prs_list[[temp]] <- prs_temp[,5:ncol(prs_temp)]
       colnames(prs_list[[temp]]) <- paste0(names,"_",
@@ -37,5 +37,6 @@ helper_combine_PRS <- function(scores,
     }
   }
   prs_mat <- as.data.frame(cbind(prs_temp[,1:2],bind_cols(prs_list)))
+  print("prs_mat complete... ")
   return(prs_mat)
 }
