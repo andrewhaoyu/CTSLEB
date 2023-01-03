7abc0712ea8715a9ee46304154b0743059fdfc16
dorks81 <dayne0208@gmail.com>
2023-01-02 20:23:37 -0500

    add step2

--- vignette_dev.R
+++ vignette_dev.R
@@ -1,4 +1,3 @@
-
 #install("~/Projects/DCEG/BB/CTSLEB_dev/CTSLEB")
 library(CTSLEB)
 library(data.table)
@@ -11,7 +10,7 @@ setwd('~/projects/CTSLEB_dev/')
 plink19_exec <- "~/Apps/plink_v1.9/plink"
 plink2_exec <- "~/Apps/plink2a/plink2"
 
-# Step 1:
+# Step 1: Two-dimensional Clumping and Thresholding (CT)
 
 # Load summary statistics data for the reference (EUR) and the target (AFR)
 # populations
@@ -20,8 +19,6 @@ sum_EUR <- fread(paste0(data.dir,"EUR_sumdata.txt"),header=T)
 sum_AFR <- fread(paste0(data.dir,"AFR_sumdata.txt"),header=T)
 head(sum_EUR)
 head(sum_AFR)
-
-# Step 1A: Alignment SNPs Across Populations
 
 library(dplyr)
 
@@ -32,21 +29,35 @@ outprefix <- "chr22"
 PRS_farm <- SetParamsFarm(plink19_exec = plink19_exec,
                           plink2_exec = plink2_exec)
 
-dimCT(results_dir = results.dir,
-      sum_target = sum_AFR,
-      sum_ref = sum_EUR,
-      ref_plink = Eur_ref_plinkfile,
-      target_plink = Afr_ref_plinkfile,
-      test_target_plink = Afr_test_plinkfile,
-      out_prefix = outprefix,
-      params_farm = PRS_farm)
+prs_mat <- dimCT(results_dir = results.dir,
+                 sum_target = sum_AFR,
+                 sum_ref = sum_EUR,
+                 ref_plink = Eur_ref_plinkfile,
+                 target_plink = Afr_ref_plinkfile,
+                 test_target_plink = Afr_test_plinkfile,
+                 out_prefix = outprefix,
+                 params_farm = PRS_farm)
 
-# pheno_file <- "data/y_tuning.txt"
-# y_tune <- fread(pheno_file)
-# PRS_tune(x = prs_mat,
-#          r2_vec = r2_vec,
-#          pthres = pthres,
-#          wc_base_vec = wc_base_vec,
-#          pheno_df = y_tune,
-#          tune_samples=10000,
-#          out = prs_out)
+pheno_tune_file <- "data/y_tuning.txt"
+y_tune <- fread(pheno_tune_file)
+prs_tune <- prs_mat[1:10000,]
+
+n.total.prs <- length(pthres)^2*length(r2_vec)*length(wc_base_vec)
+prs_r2_vec_test <- rep(0,n.total.prs)
+
+for(p_ind in 1:n.total.prs){
+  model <- lm(y_tune$V1~prs_tune[,(2+p_ind)])
+  prs_r2_vec_test[p_ind] <- summary(model)$r.square
+}
+
+max_ind <- which.max(prs_r2_vec_test)
+print(colnames(prs_tune)[max_ind+2])
+
+# Step 2: Empirical-Bayes (EB) Estimation of Effect Sizes
+
+prs_mat_eb <-EBayesEffectSize(bfile = Afr_test_plinkfile,
+                                 prs_tune = prs_tune,
+                                     plink_list = plink_list,
+                                     results_dir = results.dir,
+                              out_prefix = outprefix,
+                              params_farm = PRS_farm)
